23ede7faf60d8ef89994edfa0f03430b1b3181cf
vucongtuyen221 <vucongtuyen221@gmail.com>
2023-04-09 06:55:09 +0700

    web blog

---
+++ images/ajax/blogweb/blog.js
@@ -0,0 +1,13 @@
+ elem = document.querySelector('.nhieumon');
+var iso = new Isotope( elem, {
+  // options
+  itemSelector: '.motmon',
+  layoutMode: 'masonry'
+});
+
+// element argument can be a selector string
+//   for an individual element
+var iso = new Isotope( '.motmon', {
+  // options
+});
+
